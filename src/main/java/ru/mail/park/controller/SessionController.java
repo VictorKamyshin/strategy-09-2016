04f308a1b2581cd18bfed2c0e30770d7d0b6f067
Victor <Kamyshin.Victor@yandex.ru>
2016-12-16 00:22:21 +0300

    session controller bug

--- src/main/java/ru/mail/park/controller/SessionController.java
+++ src/main/java/ru/mail/park/controller/SessionController.java
@@ -46,9 +46,10 @@ public class SessionController {
         }
         if (user.getPassword().equals(password)) {
             UserProfile testUser = accountService.getUserById(user.getId());
-            if(testUser!=null)
-            httpSession.setAttribute("userId",user.getId()); //такое чувство, что вывод врет и по факту айдишники нумеруются с единицы
-            return new SuccessResponse("successfully_authorized");
+            if(testUser!=null) {
+                httpSession.setAttribute("userId", user.getId()); //такое чувство, что вывод врет и по факту айдишники нумеруются с единицы
+                return new SuccessResponse("successfully_authorized");
+            }
         }
         throw new ExceptionWithErrorCode("S02","wrong_login_password");
     }
